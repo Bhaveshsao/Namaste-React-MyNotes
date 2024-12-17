fdb4c45927ff03c1f82e9fcff5ff8796c41cf034
Bhavesh Sao <saobhavesh1999@gmail.com>
2024-12-17 21:24:59 +0530

    Update Chapter 4 Codes

--- App.js
+++ App.js
@@ -40,21 +40,21 @@ const resList = [
   {
     id: 1,
     resName: "Meghana Foods",
-    cuisine: "Biryani, North Indian",
+    cuisine: ['Biryani', 'North Indian'],
     rating: "4.4",
     time: "38 minutes",
   },
   {
     id: 2,
     resName: "KFC",
-    cuisine: "Chicken Wings, Burger",
+    cuisine: ['Chicken Wings', 'Burger'],
     rating: "4.7",
     time: "45 minutes",
   },
   {
     id: 3,
     resName: "Dominos",
-    cuisine: "Pizza, Italian",
+    cuisine: ['Pizza', 'Italian'],
     rating: "4.2",
     time: "30 minutes",
   },
@@ -66,7 +66,7 @@ const RestaurantCard = (props) => {
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img className="res-logo" alt="res-logo" src={swiggy} />
       <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
+      <h4>{cuisine.join(', ')}</h4>
       <h4>{rating} stars</h4>
       <h4>{time}</h4>
     </div>
